5d699c236bb81f8658945e472567a8eafecbbea0
Luphia Chang <luphia@mermer.cc>
2019-07-23 18:46:19 +0800

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'https://github.com/MerMerLtd/Cafeca-Backend',
       path: '/etc/Cafeca-Backend',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js',
     }
   }
 };
